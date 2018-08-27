b9bcf50252b1e5f58563ea49d99cb58fb7a52c95
yhp353134 <yhp353134@163.com>
2018-08-27 19:07:40 +0800

    Initial commit

---
+++ src/main/java/com/boot/test/BoottestApplication.java
@@ -0,0 +1,11 @@
+package com.boot.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoottestApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BoottestApplication.class, args);
+	}
+}
