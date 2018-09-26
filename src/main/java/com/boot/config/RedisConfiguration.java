cc47ba5c75f9652db61e124eed63136d85bde92a
yhp353134 <yhp353134@163.com>
2018-09-26 22:16:11 +0800

    添加代码

--- src/main/java/com/boot/config/RedisConfiguration.java
+++ src/main/java/com/boot/config/RedisConfiguration.java
@@ -1,6 +1,7 @@
 package com.boot.config;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
@@ -10,10 +11,25 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
 
 @EnableCaching
 @Configuration
 public class RedisConfiguration {
+
+	@Value("${spring.redis.host}")
+	private String host;
+	@Value("${spring.redis.port}")
+	private int port;
+	@Value("${spring.redis.timeout}")
+	private int timeout;
+	@Value("${spring.redis.pool.max-idle}")
+	private int maxIdle;
+	@Value("${spring.redis.pool.max-wait}")
+	private long maxWaitMillis;
+	@Value("${spring.redis.password}")
+	private String password;
 
 	@Bean
 	public CacheManager cacheManager(RedisTemplate redisTemplate) {
@@ -57,6 +73,19 @@ public class RedisConfiguration {
 		template.setValueSerializer(objectRedisSerializer);
 		template.setHashValueSerializer(objectRedisSerializer);
 		return template;
+	}
+
+	/***
+	 * 这个方法必须配置，否则将补鞥呢使用JedisPool
+	 * */
+	@Bean
+	public JedisPool redisPoolFactory() {
+		JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+		jedisPoolConfig.setMaxIdle(maxIdle);
+		jedisPoolConfig.setMaxWaitMillis(maxWaitMillis);
+		// JedisPool jedisPool = new JedisPool(jedisPoolConfig, host, port, timeout, password);
+		JedisPool jedisPool = new JedisPool(jedisPoolConfig, host, port, timeout, null);
+		return jedisPool;
 	}
 
 }
---
+++ src/main/java/com/boot/controller/TestController.java
@@ -0,0 +1,81 @@
+package com.boot.controller;
+
+import com.boot.limiter.RateLimiter;
+import com.boot.limiter.RedisRateLimiter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Controller
+@RequestMapping("/test/")
+public class TestController {
+
+	@Value("${spring.test}")
+	private String myValue;
+
+	@Autowired
+	private JedisPool jedisPool;
+
+	@Autowired
+	RedisTemplate redisTemplate;
+
+	@RequestMapping(value = "getValue")
+	@ResponseBody
+	public String getValue() {
+		return "接受到的参数为"+myValue;
+	}
+
+	@RequestMapping(value = "getRedisValue")
+	@ResponseBody
+	public String getRedisValue() {
+		redisTemplate.opsForValue().set("yu", "123");
+		String vl = redisTemplate.opsForValue().get("yu").toString();
+		System.out.println("返回的参数为："+ vl);
+		return vl;
+	}
+
+	@RateLimiter(limit = 2)
+	@RequestMapping(value = "testLimitRate")
+	public ModelAndView testLimitRate(HttpServletRequest request) {
+		return new ModelAndView("/");
+	}
+
+	@RequestMapping(value = "testRateLimiter")
+	public void testRateLimiter(HttpServletResponse response) throws IOException {
+		Jedis jedis = jedisPool.getResource();
+		/**每秒限制5个人，多了就返回系统繁忙**/
+		String token = RedisRateLimiter.acquireTokenFromBucket(jedis, 5, Long.valueOf(1000), "yu", "hai", "ping");
+		if (token == null) {
+			System.out.println("超过限流，系统繁忙");
+			response.sendError(500);
+		}else{
+			System.out.println("业务逻辑");
+		}
+		jedisPool.returnResource(jedis);
+	}
+
+	@RequestMapping(value = "testRateLimiter2")
+	public void testRateLimiter2(HttpServletResponse response) throws IOException {
+		Jedis jedis = jedisPool.getResource();
+		/**每秒限制5个人，多了就返回系统繁忙**/
+		String token = RedisRateLimiter.acquireTokenFromBucket(jedis, 5,  Long.valueOf(1000), "tan", "yao", "mei");
+		if (token == null) {
+			System.out.println("超过限流，系统繁忙");
+			response.sendError(500);
+		}else{
+			System.out.println("业务逻辑");
+		}
+		jedisPool.returnResource(jedis);
+	}
+
+}
