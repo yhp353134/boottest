32a8fc391fb24438f061e06f54904ccb208e3e81
yhp353134 <yhp353134@163.com>
2018-10-31 14:48:14 +0800

    添加代码

--- src/main/java/com/boot/thread/juc/FutureTaskTest.java
+++ src/main/java/com/boot/thread/juc/FutureTaskTest.java
@@ -1,15 +1,16 @@
 package com.boot.thread.juc;
 
-import java.util.concurrent.Callable;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.FutureTask;
+import java.util.concurrent.*;
 
 public class FutureTaskTest {
 
 
+    private static ExecutorService executorService = Executors.newCachedThreadPool();
+
     public static void main(String[] args) throws InterruptedException, ExecutionException {
-        /*FutureTask<String> futureTask = new FutureTask<String>(new Callable<String>() {
+      /*
+
+       FutureTask<String> futureTask = new FutureTask<String>(new Callable<String>() {
             @Override
             public String call() throws Exception {
                 Thread.sleep(5000);
@@ -20,12 +21,10 @@ public class FutureTaskTest {
         new Thread(futureTask).start();
         Thread.sleep(1000);
         System.out.println("处理逻辑");
-        String str = futureTask.get(); // 这里会阻塞
+
+
+         String str = futureTask.get(); // 这里会阻塞
         System.out.println(str);
-
-
-
-
         ExecutorService executorService = Executors.newCachedThreadPool();
           Future future = executorService.submit(new Callable<Object>() {
               @Override
@@ -46,7 +45,7 @@ public class FutureTaskTest {
              e.printStackTrace();
          }
 */
-        CompletableFuture<String> future = CompletableFuture.supplyAsync(() -> "Hello")
+       /* CompletableFuture<String> future = CompletableFuture.supplyAsync(() -> "Hello")
                 .thenApply(String::toUpperCase)
                 .thenApply(r-> r+"123")
                 .thenApply(r-> r+"你好")
@@ -54,7 +53,59 @@ public class FutureTaskTest {
                 // .thenAccept(System.out::println()print(r);)
                 .whenComplete((i,e)-> {
                     System.out.println(i);
-                });
+                });*/
+
+
+     /*
+     run方法会阻塞
+     FutureTask<String> futureTask = new FutureTask<String>(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                System.out.println("任务执行了");
+                Thread.sleep(5000);
+                // 里面是异步处理的逻辑
+                return "done";
+            }
+        });
+        futureTask.run();  // 这里会阻塞
+        System.out.println("执行完成后");
+
+        // 线程池使用不会阻塞
+        FutureTask<String> futureTask = new FutureTask<String>(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                System.out.println("任务执行了");
+                Thread.sleep(5000);
+                System.out.println("任务执行了之后");
+                // 里面是异步处理的逻辑
+                return "done";
+            }
+        });
+        executorService.submit(futureTask);
+        System.out.println("执行完成了");
+
+
+        */
+
+
+        // 下面的方法不会阻塞
+       /* Future<String> future = executorService.submit(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                System.out.println("任务执行了");
+                Thread.sleep(5000);
+                System.out.println("任务执行了之后");
+                // 里面是异步处理的逻辑
+                return "done";
+            }
+        });
+        System.out.println("执行完成后");*/
+
+
+
+
+
+       // main 结束
     }
 
 
